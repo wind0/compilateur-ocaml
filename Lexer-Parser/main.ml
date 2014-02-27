4f91c6c8d182ba9e69e66a1cc0ca40a3364d4721
wind0 <windorigin@live.fr>
2014-02-27 20:35:33 +0100

    Exemple juste pour toi Issam

--- Lexer-Parser/main.ml
+++ Lexer-Parser/main.ml
@@ -1,4 +1,6 @@
 let chan = open_in "Test_file/test_0.2.5.pas"
 let lexbuf = Lexing.from_channel chan
 
-let _ = ParserHELIX.program Lexer.token lexbuf
+let truc = ParserHELIX.program Lexer.token lexbuf
+
+let _ = Printf.printf "%s" truc.prog_name
