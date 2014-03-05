78107ea7855a28cf06d9632fa8efda0c95c3b6f0
wind0 <windorigin@live.fr>
2014-03-05 11:14:14 +0100

    BAM get_const!!!! TAKE SOME CODE!

--- Lexer-Parser/typecheck.ml
+++ Lexer-Parser/typecheck.ml
@@ -8,11 +8,36 @@ type const =
 
 let table_symbol = symbol list
 
+(*return a symbol option*)
+(*manque pas mal de trucs, tableaux, strings et records*)
+let get_init_var = fun ivar ->
+match ivar with
+|(id, Simple (Type_identifier ty)) -> Some (id, ty)
+|_ -> None
+
+
+
+(*identifier * const*)
+let get_const = fun iconst ->
+match iconst with
+|(id, c) -> 	let value = begin 
+		match c with 
+		| SignedIdOrNumber (s, BInteger bi) -> 	if s == Minus then - bi else bi
+		| IdOrNumber (BInteger bi) -> bi	
+		end
+		in	
+		Some(Cinteger((id,TypInteger),value))
+|_ -> None
+
+
 let rec create_table_rec = fun bl_var res ->
 match bl_var with
-|(id, Simple (Type_identifier ty))::t -> create_table_rec t ((id, ty)::res)
+|h::t -> begin 
+	match get_init_var with 
+	|Some truc	-> create_table_rec t (truc::res)
+	|None -> create_table_rec t res
+	end 
 |[] -> res
-|_::t ->  create_table_rec bl_var res
 |_ -> failwith ("Impossible error?")  
 
 let create_table = fun bl_var ->
