268646b95edae5539151e3f3af01e0948665386c
wind0 <windorigin@live.fr>
2014-03-09 18:13:36 +0100

    le typecheck a l'air de marcher, l'ast est corrigé. Le tout est très sommaire, mais... FUCK YEAH!

--- Lexer-Parser/main.ml
+++ Lexer-Parser/main.ml
@@ -1,4 +1,5 @@
 open Astprinter
+open Typecheck
 
 let chan = open_in "Test_file/test_0.2.5.pas"
 let lexbuf = Lexing.from_channel chan
@@ -7,3 +8,5 @@ let magical_pamyu = ParserHELIX.program Lexer.token lexbuf
 
 
 let i_love_jelly_babies  = Astprinter.print magical_pamyu "foulemoila.dot"
+
+let _ = Printf.printf "%B\n" (Typecheck.typc_prog magical_pamyu)
